50f8c31033b8c47ae93cc66b8a077bb2d4a1c445
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-05-23 19:20:27 +0930

    get request and response

--- server.js
+++ server.js
@@ -8,3 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));

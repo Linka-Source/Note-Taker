f3dac90ba183b95a81a466396211cb8308a0d9c5
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-05-23 19:06:59 +0930

    added express for parsing

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));

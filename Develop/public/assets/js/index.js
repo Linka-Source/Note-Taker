50ffe4b3334c4fb26d07662caac38b80f4152e98
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-05-23 20:01:41 +0930

    deleted what's been refactored

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -35,51 +35,7 @@ const editNote = function(id) {
   })
 };
 
-// if (window.location.pathname === '/notes') {
-//   noteTitle = document.querySelector('.note-title');
-//   noteText = document.querySelector('.note-textarea');
-//   saveNoteBtn = document.querySelector('.save-note');
-//   newNoteBtn = document.querySelector('.new-note');
-//   noteList = document.querySelectorAll('.list-container .list-group');
-// }
 
-// // Show an element
-// const show = (elem) => {
-//   elem.style.display = 'inline';
-// };
-
-// // Hide an element
-// const hide = (elem) => {
-//   elem.style.display = 'none';
-// };
-
-// // activeNote is used to keep track of the note in the textarea
-// let activeNote = {};
-
-// const getNotes = () =>
-//   fetch('/api/notes', {
-//     method: 'GET',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   });
-
-// const saveNote = (note) =>
-//   fetch('/api/notes', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(note),
-//   });
-
-// const deleteNote = (id) =>
-//   fetch(`/api/notes/${id}`, {
-//     method: 'DELETE',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   });
 
 // const renderActiveNote = () => {
 //   hide(saveNoteBtn);

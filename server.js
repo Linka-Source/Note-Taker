57f2a0338de003553f8c3481ded1886fc91df193
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-05-24 14:27:05 +0930

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
